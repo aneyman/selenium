b40fa125e6f1e4b8fa65e16bb667872966ee1d24
Aleks Neyman <alleksiy.n@gmail.com>
2018-01-14 19:37:13 +0200

    login test

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,6 +1,7 @@
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -22,7 +23,11 @@ public class FirstTest {
     public void myFirstTest() throws InterruptedException {
         driver.get("http://localhost/litecart/admin/");
 //        driver.get("https://google.com/");
+        TimeUnit.SECONDS.sleep(2);
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.xpath("//*[@id=\"box-login\"]/form/div[2]/button")).click();
         TimeUnit.SECONDS.sleep(5);
 //        driver.findElement(By.name("q")).sendKeys("webdriver", Keys.ENTER);
 //        wait.until(titleIs("webdriver - Поиск в Google"));
